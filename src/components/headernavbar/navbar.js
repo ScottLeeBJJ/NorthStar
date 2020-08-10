46c717fc5f2ed406bdc4dee9d618d16aacb37538
ScottLeeBJJ <scottlee5455@outlook.com>
2020-08-09 22:32:56 -0700

    finishing up before launch

---
+++ src/components/headernavbar/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div className="navbar">
+                <h1>Home</h1>
+                <h1>Testimonials</h1>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
